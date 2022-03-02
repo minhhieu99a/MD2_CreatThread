5be569edcbdc099e39f7e3555e237edaa29831bb
minhhieu99a <hieunb99@gmail.com>
2022-03-02 15:14:46 +0700

    done

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         NumberGenerator numberGenerator=new NumberGenerator();
-        numberGenerator.set
         numberGenerator.start();
         NumberGenerator numberGenerator1=new NumberGenerator();
         numberGenerator1.start();
